04420f520c1944ce044787028a13d3bba8b23ce9
myonaingwinn <myonaingwinn081@gmail.com>
2023-05-15 14:32:44 +0900

    add : dist

---
+++ assets/AboutView.f81acd08.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index.eaa2eee8.js";const t={};var s=function(){var n=this;return n._self._c,n._m(0)},a=[function(){var _=this,n=_._self._c;return n("div",{staticClass:"about"},[n("h1",[_._v("This is an about page")])])}],r=e(t,s,a,!1,null,null,null,null);const l=r.exports;export{l as default};
