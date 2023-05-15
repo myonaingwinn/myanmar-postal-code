31b1fb253b349a7f0df728672c72fa60171656dd
myonaingwinn <myonaingwinn081@gmail.com>
2023-05-15 15:07:02 +0900

    fix : eslint

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import { createPinia, PiniaVuePlugin } from 'pinia'
+import Vue from "vue";
+import { createPinia, PiniaVuePlugin } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-Vue.use(PiniaVuePlugin)
+Vue.use(PiniaVuePlugin);
 
 new Vue({
   router,
   pinia: createPinia(),
-  render: (h) => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
