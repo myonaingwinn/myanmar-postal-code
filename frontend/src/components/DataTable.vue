bf673b9791d2784199527a5d8b7600fbcafacb83
myonaingwinn <myonaingwinn081@gmail.com>
2023-05-23 14:47:14 +0900

    add : pagination

---
+++ frontend/src/components/DataTable.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: ['tableData'],
+};
+</script>
+<template>
+  <el-table
+    :data="this.tableData"
+    stripe
+    border
+    style="width: 80%"
+    class="data-table"
+  >
+    <el-table-column prop="Region" label="Region"> </el-table-column>
+    <el-table-column prop="Town / Township" label="Town / Township">
+    </el-table-column>
+    <el-table-column
+      prop="Quarter / Village Tract"
+      label="Quarter / Village Tract"
+    >
+    </el-table-column>
+    <el-table-column
+      prop="Postal Code"
+      label="Postal Code"
+      @cell-click="() => console.log('Hi')"
+    >
+    </el-table-column>
+  </el-table>
+</template>
--- frontend/src/components/SearchForm.vue
+++ frontend/src/components/SearchForm.vue
@@ -1,40 +1,14 @@
 <script>
-import axios from 'axios';
-
 export default {
   name: 'SearchForm',
   data() {
     return {
       keyword: '',
-      tableData: [],
     };
   },
-  methods: {
-    search() {
-      if (this.keyword) {
-        axios
-          .get(import.meta.env.VITE_BASE_URL + '/search', {
-            params: this.axiosParams,
-          })
-          .then((res) => {
-            if (res.data) this.tableData = res.data;
-          })
-          .catch((error) => {
-            console.error('err', error);
-          });
-      }
-
-      this.tableData = [];
-    },
-  },
-  computed: {
-    axiosParams() {
-      const params = new URLSearchParams();
-      params.append('keyword', this.keyword);
-      params.append('lang', 'en');
-      params.append('page', 1);
-      params.append('pageSize', 10);
-      return params;
+  watch: {
+    keyword: function () {
+      this.$emit('setKeyword', this.keyword);
     },
   },
 };
@@ -47,32 +21,7 @@ export default {
       v-model="keyword"
       prefix-icon="el-icon-search"
       clearable
-      @input="search"
     >
     </el-input>
-
-    <el-table
-      :data="tableData"
-      stripe
-      border
-      style="width: 80%"
-      class="data-table"
-    >
-      <el-table-column prop="Region" label="Region"> </el-table-column>
-      <el-table-column prop="Town / Township" label="Town / Township">
-      </el-table-column>
-      <el-table-column
-        prop="Quarter / Village Tract"
-        label="Quarter / Village Tract"
-      >
-      </el-table-column>
-      <el-table-column
-        prop="Postal Code"
-        label="Postal Code"
-        @cell-click="() => console.log('Hi')"
-      >
-      </el-table-column>
-    </el-table>
-    <!-- TODO: add pagination -->
   </div>
 </template>
