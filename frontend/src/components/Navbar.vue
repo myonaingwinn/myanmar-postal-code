24559fe13a792ad5467e002c9b9fa6f38c988bd8
myonaingwinn <myonaingwinn081@gmail.com>
2023-05-31 10:32:27 +0900

    fix : Navbar design and code

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,21 +1,30 @@
 <template>
   <div class="navbar">
     <el-row>
-      <el-col :xs="17" :sm="16" :md="17" :lg="18" :xl="20">
+      <el-col :xs="16" :sm="15" :md="17" :lg="19" :xl="19">
         <div class="app-name">Myanmar Postal Code</div>
       </el-col>
-      <el-col :xs="0" :sm="4" :md="3" :lg="3" :xl="2" class="nav-col">
+      <el-col :xs="0" :sm="4" :md="3" :lg="2" :xl="2" class="nav-col">
         <el-dropdown @command="changeLanguage" size="medium" split-button>
           <span class="el-dropdown-link">
             {{ currentLanguage.toUpperCase() }}
           </span>
           <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item command="en">EN</el-dropdown-item>
-            <el-dropdown-item command="mm" divided>MM</el-dropdown-item>
+            <el-dropdown-item
+              :disabled="currentLanguage === LANGUAGE.EN"
+              :command="LANGUAGE.EN"
+              >EN</el-dropdown-item
+            >
+            <el-dropdown-item
+              :disabled="currentLanguage === LANGUAGE.MM"
+              :command="LANGUAGE.MM"
+              divided
+              >MM</el-dropdown-item
+            >
           </el-dropdown-menu>
         </el-dropdown>
       </el-col>
-      <el-col :xs="7" :sm="4" :md="4" :lg="3" :xl="2" class="nav-col">
+      <el-col :xs="8" :sm="5" :md="4" :lg="3" :xl="3" class="nav-col">
         <el-dropdown size="medium" split-button>
           {{ $t('navbar.help') }}
           <el-dropdown-menu slot="dropdown">
@@ -38,6 +47,7 @@ export default {
   data() {
     return {
       language: this.$pinia.state.currentLanguage,
+      LANGUAGE,
     };
   },
 
